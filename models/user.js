6be8cce90fdff7846682c691fdf7d6b53e8b1001
Govard9 <web-supsu@yandex.ru>
2023-05-08 16:45:53 +0700

    feat: Добавить в схему User поле email и password. Прописать им нужные свойства.

--- models/user.js
+++ models/user.js
@@ -3,6 +3,17 @@ const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
+    email: {
+      required: [true, 'Поле name обязательно к заполнению.'],
+      unique: [true, 'Такой email уже использует кто-то другой.'],
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: 'Некорректный URL.',
+      },
+    },
+    password: {
+      required: [true, 'Поле name обязательно к заполнению.'],
+    },
     name: {
       type: String,
       required: [true, 'Поле name обязательно к заполнению.'],
