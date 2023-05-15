8f1cfd9fa838ec1d5baed2026bd70211feda175c
govard9 <web-supsu@yandex.ru>
2023-05-15 15:57:46 +0700

    feat: Добавление валидации celebrate и Joi.

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,9 @@
-const router = require('express').Router();
+const router = require('express')
+  .Router();
+const {
+  celebrate,
+  Joi,
+} = require('celebrate');
 const auth = require('../middlewares/auth');
 
 const {
@@ -11,8 +16,34 @@ const {
   createUser,
 } = require('../controllers/users');
 
-router.post('/signin', login);
-router.post('/signup', createUser);
+router.post('/signin', celebrate({
+  body: Joi.object()
+    .keys({
+      email: Joi.string()
+        .required()
+        .email(),
+      password: Joi.string()
+        .required(),
+    }),
+}), login);
+
+router.post('/signup', celebrate({
+  body: Joi.object()
+    .keys({
+      email: Joi.string()
+        .required()
+        .email(),
+      password: Joi.string()
+        .required(),
+      name: Joi.string()
+        .min(2)
+        .max(30),
+      about: Joi.string()
+        .min(2)
+        .max(30),
+      avatar: Joi.string(),
+    }),
+}), createUser);
 
 router.get('/users', auth, getUser);
 router.get('/users/me', auth, getUserMe);
