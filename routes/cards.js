24f7d2bdc015579dfd5c673223717ac5ae420a7a
govard9 <web-supsu@yandex.ru>
2023-05-18 22:06:08 +0700

    feat: Прохождение автотестов.

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,29 @@
 const router = require('express').Router();
+const {
+  celebrate,
+  Joi,
+} = require('celebrate');
 const auth = require('../middlewares/auth');
-
 const {
   getCard, cardDelete, createCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/cards', auth, getCard);
-router.delete('/cards/:id', auth, cardDelete);
+router.delete('/cards/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().hex().length(24),
+  }),
+}), auth, cardDelete);
 router.post('/cards', auth, createCard);
-router.put('/cards/:id/likes', auth, likeCard);
-router.delete('/cards/:id/likes', auth, dislikeCard);
+router.put('/cards/:id/likes', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().hex().length(24),
+  }),
+}), auth, likeCard);
+router.delete('/cards/:id/likes', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().hex().length(24),
+  }),
+}), auth, dislikeCard);
 
 module.exports = router;
